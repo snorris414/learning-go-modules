2886d54949ba0d75c158aeb768319fc0e8a49dc4
snorris414 <snorris414@gmail.com>
2024-07-01 21:00:10 -0500

    Making Add function in math package generic

--- math/go.mod
+++ math/go.mod
@@ -1,3 +1,7 @@
 module github.com/snorris414/learning-go-modules
 
 go 1.22.4
+
+require (
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
+)
--- math/math.go
+++ math/math.go
@@ -1,8 +1,14 @@
 // math provides functions for doing basic math operations.
 package math
 
-// Add takes 2 integers and returns their sum
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add takes 2 Numbers and returns their sum
 // For more information about adding numbers, see: https://www.mathisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
