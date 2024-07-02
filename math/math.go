0b5d1e9f165f61a13b57dfc340d4ac11683f3c7f
snorris414 <snorris414@gmail.com>
2024-07-01 20:42:35 -0500

    Adding comments for godoc for math package and Add function

--- math/math.go
+++ math/math.go
@@ -1,5 +1,8 @@
+// math provides functions for doing basic math operations.
 package math
 
+// Add takes 2 integers and returns their sum
+// For more information about adding numbers, see: https://www.mathisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
